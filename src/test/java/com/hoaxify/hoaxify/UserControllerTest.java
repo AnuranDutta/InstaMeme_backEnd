972fc6727cab16e5e4b39b96d587ffcb012b5587
Anuran <anuran32z@gmail.com>
2023-03-31 03:16:10 +0530

    Section 3: 15. Password Hashing (Backend)

--- src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.hoaxify.hoaxify;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -16,8 +18,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.hoaxify.hoaxify.shared.GenericResponse;
 import com.hoaxify.hoaxify.user.User;
 import com.hoaxify.hoaxify.user.UserRepository;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -52,6 +56,22 @@ public class UserControllerTest {
 		User user = createValidUser();
 		testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
 		assertThat(userRepository.count()).isEqualTo(1);
+	}
+	
+	@Test
+	public void postUser_whenUserIsValid_receiveSuccessMessage() {
+		User user = createValidUser();
+		ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
+		assertThat(response.getBody().getMessage()).isNotNull();
+	}
+	
+	@Test
+	public void postUser_whenUserIsValid_passwordIsHashedInDatabase() {
+		User user = createValidUser();
+		testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+		List<User> users = userRepository.findAll();
+		User inDB = users.get(0);
+		assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
 	}
 	
 	private User createValidUser() {
